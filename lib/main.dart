ab3a70f3970d522a1f88940298153f3043e56af0
ZERONE <zerone012@gmail.com>
2023-08-28 09:35:36 +0900

    UI 공부 중. 상태변경 - GetX 중 Reactive State Manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:study_flutter_13_totally03_lec_reactivestate/student_controller.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  final StudentController _con = Get.put(StudentController());
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter Demo Home Page')),
-        body: const Center(
-          child: Column(
-            children: [],
-          ),
+        appBar: AppBar(title: const Text('GetX Sample')),
+        body: ListView.builder(
+          itemCount: _con.studentList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              margin: const EdgeInsets.all(15),
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  GetX<StudentController>(
+                    builder: (_) => Text(
+                        "ID: ${_con.studentList[index]().studentId}, Name: ${_con.studentList[index]().studentName}, Grade: ${_con.studentList[index]().studentGrade}"),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () => _con.updateStudentName(
+                            _con.newStudentNames[index], index),
+                        child: const Text('이름 변경'),
+                      ),
+                      TextButton(
+                        onPressed: () => _con.updateStudentGrade(
+                            _con.newStudentGrades[index], index),
+                        child: const Text('성적 변경'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
